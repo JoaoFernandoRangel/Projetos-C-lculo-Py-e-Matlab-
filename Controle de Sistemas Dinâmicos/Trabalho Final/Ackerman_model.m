096dbd732faa0a1381b082611a8864eedd2f7830
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-06-24 11:57:31 -0300

    a

--- Controle de Sistemas Dinâmicos/Trabalho Final/Ackerman_model.m
+++ Controle de Sistemas Dinâmicos/Trabalho Final/Ackerman_model.m
@@ -1,57 +1,28 @@
 clear all
+close all 
 clc
-hold on
-%{
-Separar o script em uma máquina de estados
-Colocar MUX de seleção de cada estado
-Prever os estados, o input será o lado do quadrado.
-Generalizar e otimizar posteriormente.
-%}
+
+State = [0 0 0];
+s = tf('s');
+FT = 400/(s+20); % Entrada em Volts, Saída em Rad/s
+%Características do Carro
+Raio_roda_direita = 0.065;
+Raio_roda_esquerda= 0.065;
+Eixo = 0.2;
+Raios_media = (Raio_roda_esquerda + Raio_roda_direita)/2;
+VelR = FT; %Adicionar Feedback e PID
+VelL = FT; %Adicionar feedback e PID
+%Cálculo dos valores.
+Theta = (1/s)*(Raios_media/(2*Eixo))*(VelR-VelL)*(0.5);
+X = (Raios_media*(VelR+VelL))/(2*(s^2) + 2*(Theta^2));
+Y = (Raios_media*Theta*(VelR+VelL))/(2*(s^3) + 2*s*(Theta^2));
+State = [X Y Theta]
+step(15*feedback(X,1));
 
 
-% Parameters
-WheelRadius = 0.065/2; % Wheel radius in meters
-TrackWidth = 0.02;  % Distance between the wheels (track width) in meters
-lado = 2;
-% Define the kinematic model
-kinematicModel = differentialDriveKinematics('WheelRadius', WheelRadius, 'TrackWidth', TrackWidth);
-
-% Initial conditions
-ini_pos_x =  0;
-ini_pos_y = 0;
-angulo = 0;
-ini_orientation = atan(angulo*pi/180); % Initial orientation in radians
-
-% Wheel speeds
-W_speed_L = 30; % Left wheel speed in rad/s
-W_speed_R = 30;  % Right wheel speed in rad/s
-W_media = (W_speed_L + W_speed_R)/2;
-% Initial state [x position, y position, orientation]
-initialState = [ini_pos_x ini_pos_y ini_orientation];
 
 
-if (W_media ~= 0)
-    tempo_final = lado/(WheelRadius*W_media);
-else
-    tempo_final = 1;
-end 
-% Time span for the simulation
-tspan = 0:0.05:tempo_final;
-
-% Inputs (wheel speeds)
-inputs = [W_speed_L W_speed_R]; % Left and right wheel speeds
-
-% ODE solver to simulate the differential drive kinematics
-[t, y] = ode45(@(t, y) derivative(kinematicModel, y, inputs), tspan, initialState);
-
-% Plotting the trajectory
-Xlim = [-(lado+1) lado+1];
-Ylim = [-(lado+1) lado+1];
 
 
-plot(y(:, 1), y(:, 2))
-axis([Xlim Ylim]); % Define the axis limits
-grid on;
-xlabel('X Position');
-ylabel('Y Position');
-title('Differential Drive Kinematics');
+
+
